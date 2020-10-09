9fcc010cb3bd58664b69b203b93441bb8798b4e8
Daniel Moll <dmoll1974@gmail.com>
2020-10-09 09:52:36 +0200

    updated network name

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ docker-compose --compatibility up -d --remove-orphans mongo{1,2,3}
 
 echo "Configuring Mongo replica-set..."
 sleep 1
-$DOCKER_CMD run -ti --rm -v $CONFIG_FILE:/init-mongo.js --net perfana-demo_perfana mongo:4.4-rc /usr/bin/mongo --host mongo1 --port 27011 /init-mongo.js
+$DOCKER_CMD run -ti --rm -v $CONFIG_FILE:/init-mongo.js --net perfana-blog_perfana mongo:4.4-rc /usr/bin/mongo --host mongo1 --port 27011 /init-mongo.js
 
 echo "Sleeping for ${PAUSE} secs to give Mongo a chance to start the replicaset..."
 sleep ${PAUSE}
